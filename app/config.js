e617ba14981bb72b294e40e29885af5e1957cc52
“dimmchs” <Dimitris.chatzichristos@engineering.digital.dwp.gov.uk>
2022-02-03 11:10:47 +0000

    First commit

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Check a payment failure',
+  serviceName: 'Check an unsent payment',
 
   // Default port that prototype runs on
   port: '3000',
